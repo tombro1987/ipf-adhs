3c045bd21d5ebe1e2fa4e9f343d13e69d49e9bf0
philmikejones <philmikejones+github@gmail.com>
2014-03-28 14:33:03 +0000

    next step: need to sum each survey variable into col totals
    Signed-off-by: philmikejones <>

--- adhs.R
+++ adhs.R
@@ -25,42 +25,42 @@ summary(adhs$Treat)
 
 # Prepare outcome variables in survey file ====
 # Sex
-adhs$male <- NA
-adhs$male[adhs$Sex == 1] <- 1
-adhs$male[adhs$Sex == 2] <- 0
-adhs$female <- NA
-adhs$female[adhs$Sex == 2] <- 1
-adhs$female[adhs$Sex == 1] <- 0
+adhs$male_s <- NA
+adhs$male_s[adhs$Sex == 1] <- 1
+adhs$male_s[adhs$Sex == 2] <- 0
+adhs$female_s <- NA
+adhs$female_s[adhs$Sex == 2] <- 1
+adhs$female_s[adhs$Sex == 1] <- 0
 
 # NSSEC8
 adhs$NSSEC8[adhs$NSSEC8 == 97] <- NA
-adhs$ns11 <- NA
-adhs$ns12 <- NA
-adhs$ns2  <- NA
-adhs$ns3  <- NA
-adhs$ns4  <- NA
-adhs$ns5  <- NA
-adhs$ns6  <- NA
-adhs$ns7  <- NA
-adhs$ns8  <- NA
-adhs$ns11[adhs$NSSEC8 == 1.1] <- 1
-adhs$ns11[adhs$NSSEC8 >  1.1] <- 0
-adhs$ns12[adhs$NSSEC8 == 1.2] <- 1
-adhs$ns12[adhs$NSSEC8 == 1.1 | adhs$NSSEC8 > 1.2] <- 0
-adhs$ns2[adhs$NSSEC8 == 2]   <- 1
-adhs$ns2[adhs$NSSEC8 < 2 | adhs$NSSEC8 > 2]   <- 0
-adhs$ns3[adhs$NSSEC8 == 3]   <- 1
-adhs$ns3[adhs$NSSEC8 < 3 | adhs$NSSEC8 > 3]   <- 0
-adhs$ns4[adhs$NSSEC8 == 4]   <- 1
-adhs$ns4[adhs$NSSEC8 < 4 | adhs$NSSEC8 > 4]   <- 0
-adhs$ns5[adhs$NSSEC8 == 5]   <- 1
-adhs$ns5[adhs$NSSEC8 < 5 | adhs$NSSEC8 > 5]   <- 0
-adhs$ns6[adhs$NSSEC8 == 6]   <- 1
-adhs$ns6[adhs$NSSEC8 < 6 | adhs$NSSEC8 > 6]   <- 0
-adhs$ns7[adhs$NSSEC8 == 7]   <- 1
-adhs$ns7[adhs$NSSEC8 < 7 | adhs$NSSEC8 > 7]   <- 0
-adhs$ns8[adhs$NSSEC8 == 8]   <- 1
-adhs$ns8[adhs$NSSEC8 < 8 | adhs$NSSEC8 > 8]   <- 0
+adhs$ns11_s <- NA
+adhs$ns12_s <- NA
+adhs$ns2_s  <- NA
+adhs$ns3_s  <- NA
+adhs$ns4_s  <- NA
+adhs$ns5_s  <- NA
+adhs$ns6_s  <- NA
+adhs$ns7_s  <- NA
+adhs$ns8_s  <- NA
+adhs$ns11_s[adhs$NSSEC8 == 1.1] <- 1
+adhs$ns11_s[adhs$NSSEC8 >  1.1] <- 0
+adhs$ns12_s[adhs$NSSEC8 == 1.2] <- 1
+adhs$ns12_s[adhs$NSSEC8 == 1.1 | adhs$NSSEC8 > 1.2] <- 0
+adhs$ns2_s[adhs$NSSEC8 == 2]   <- 1
+adhs$ns2_s[adhs$NSSEC8 < 2 | adhs$NSSEC8 > 2]   <- 0
+adhs$ns3_s[adhs$NSSEC8 == 3]   <- 1
+adhs$ns3_s[adhs$NSSEC8 < 3 | adhs$NSSEC8 > 3]   <- 0
+adhs$ns4_s[adhs$NSSEC8 == 4]   <- 1
+adhs$ns4_s[adhs$NSSEC8 < 4 | adhs$NSSEC8 > 4]   <- 0
+adhs$ns5_s[adhs$NSSEC8 == 5]   <- 1
+adhs$ns5_s[adhs$NSSEC8 < 5 | adhs$NSSEC8 > 5]   <- 0
+adhs$ns6_s[adhs$NSSEC8 == 6]   <- 1
+adhs$ns6_s[adhs$NSSEC8 < 6 | adhs$NSSEC8 > 6]   <- 0
+adhs$ns7_s[adhs$NSSEC8 == 7]   <- 1
+adhs$ns7_s[adhs$NSSEC8 < 7 | adhs$NSSEC8 > 7]   <- 0
+adhs$ns8_s[adhs$NSSEC8 == 8]   <- 1
+adhs$ns8_s[adhs$NSSEC8 < 8 | adhs$NSSEC8 > 8]   <- 0
 
 
 
@@ -77,7 +77,7 @@ keep <- c("geography.code",
           "Sex..Females..NS.SeC..All.categories..NS.SeC..measures..Value")
 cen <- cen[keep]
 rm(keep)
-names(cen) <- c("lsoacode", "male", "female")
+names(cen) <- c("lsoacode", "male_c", "female_c")
 cen <- as.data.frame(cen)
 
 # NSSEC8
@@ -98,17 +98,39 @@ keep <- c("geography.code",
           "Sex..All.persons..NS.SeC..8..Never.worked.and.long.term.unemployed..measures..Value")
 sec <- sec[keep]
 rm(keep)
-names(sec) <- c("lsoacode", "ns11", "ns12", "ns2", "ns3", "ns4", 
-                "ns5", "ns6", "ns7", "ns8")
+names(sec) <- c("lsoacode", "ns11_c", "ns12_c", "ns2_c", "ns3_c", "ns4_c", 
+                "ns5_c", "ns6_c", "ns7_c", "ns8_c")
 cen <- merge(cen, sec, by.x = "lsoacode", by.y = "lsoacode")
 rm(sec)
-
-keep <- c("Serial", "SHA", "Treat", "male", "female",
-          "ns11", "ns12", "ns2", "ns3", "ns4", "ns5", "ns6", "ns7", "ns8")
+View(adhs)
+keep <- c("Serial", "SHA", "Treat", "male_s", "female_s",
+          "ns11_s", "ns12_s", "ns2_s", "ns3_s", "ns4_s", "ns5_s", 
+          "ns6_s", "ns7_s", "ns8_s")
 adhs <- adhs[keep]
 rm(keep)
 
 adhs <- na.omit(adhs)
 adhs <- as.data.frame(adhs)
-View(adhs)
-View(cen)
+
+adhs$key <- 999
+cen$key <- 999
+
+adhs.yh <- adhs[adhs$SHA == 3, ]
+cen.s <- cen[sample(1:nrow(cen), 1000, replace = F), ]
+rm(adhs, cen)
+
+apply(adhs.yh[c("Treat",  "male_s",	"female_s",	"ns11_s",	"ns12_s",	"ns2_s",
+              "ns3_s",	"ns4_s",	"ns5_s",	"ns6_s",	"ns7_s",	"ns8_s")],
+      MARGIN = 2,
+      sum)
+
+# lsoa1 <- merge(adhs.yh, cen.s[1, ], by.x = "key", by.y = "key")
+# lsoa1$wt0 <- 1
+# varlist <- c("male_s",  "female_s"	ns11	ns12	ns2	ns3	ns4	ns5	ns6	ns7	ns8)
+# for (i in varlist){
+#   #
+# }
+
+
+
+# apply(cen, MARGIN = 1, merge(adhs, cen, by = "key"))
